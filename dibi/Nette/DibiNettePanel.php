cf2e08c20f452ae3babdc8a4069d87efc3262c84
David Grudl <david@grudl.com>
2012-02-01 21:50:26 +0100

    DibiNettePanel: fixed doubled alias

--- dibi/Nette/DibiNettePanel.php
+++ dibi/Nette/DibiNettePanel.php
@@ -49,7 +49,7 @@ class DibiNettePanel extends DibiObject implements IBarPanel
 
 	public function register(DibiConnection $connection)
 	{
-		if (is_callable('Nette\Diagnostics\Debugger::enable')) {
+		if (is_callable('Nette\Diagnostics\Debugger::enable') && !class_exists('NDebugger')) {
 			class_alias('Nette\Diagnostics\Debugger', 'NDebugger'); // PHP 5.2 code compatibility
 		}
 		if (is_callable('NDebugger::enable')) {
