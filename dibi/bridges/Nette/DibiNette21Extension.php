e5b2ca46d83676b6b9a89a8ec19104393cf94819
David Grudl <david@grudl.com>
2013-06-22 18:12:20 +0200

    added extension for Nette Framework 2.1 (and examples) [Closes #93]

---
+++ dibi/bridges/Nette/DibiNette21Extension.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of the "dibi" - smart database abstraction layer.
+ *
+ * Copyright (c) 2005 David Grudl (http://davidgrudl.com)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+
+
+/**
+ * Dibi extension for Nette Framework 2.1. Creates 'connection' service.
+ *
+ * @author     David Grudl
+ * @package    dibi\nette
+ * @phpversion 5.3
+ */
+class DibiNette21Extension extends Nette\DI\CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$container = $this->getContainerBuilder();
+		$config = $this->getConfig();
+
+		$useProfiler = isset($config['profiler'])
+			? $config['profiler']
+			: !$container->parameters['productionMode'];
+
+		unset($config['profiler']);
+
+		if (isset($config['flags'])) {
+			$flags = 0;
+			foreach ((array) $config['flags'] as $flag) {
+				$flags |= constant($flag);
+			}
+			$config['flags'] = $flags;
+		}
+
+		$connection = $container->addDefinition($this->prefix('connection'))
+			->setClass('DibiConnection', array($config));
+
+		if ($useProfiler) {
+			$panel = $container->addDefinition($this->prefix('panel'))
+				->setClass('DibiNettePanel')
+				->addSetup('Nette\Diagnostics\Debugger::getBar()->addPanel(?)', array('@self'))
+				->addSetup('Nette\Diagnostics\Debugger::getBlueScreen()->addPanel(?)', array(array('@self', 'renderException')));
+
+			$connection->addSetup('$service->onEvent[] = ?', array(array($panel, 'logEvent')));
+		}
+	}
+
+}
