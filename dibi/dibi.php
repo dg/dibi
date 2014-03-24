5614806088dbaced548ce533fee020a4cdd7f16e
David Grudl <david@grudl.com>
2014-03-24 19:25:32 +0100

    bridges: changed file structure

--- dibi/dibi.php
+++ dibi/dibi.php
@@ -32,7 +32,7 @@ require_once dirname(__FILE__) . '/libs/DibiEvent.php';
 require_once dirname(__FILE__) . '/libs/DibiFileLogger.php';
 require_once dirname(__FILE__) . '/libs/DibiFirePhpLogger.php';
 if (interface_exists('Nette\Diagnostics\IBarPanel') || interface_exists('IBarPanel')) {
-	require_once dirname(__FILE__) . '/bridges/Nette/DibiNettePanel.php';
+	require_once dirname(__FILE__) . '/bridges/Nette-2.1/DibiNettePanel.php';
 }
 
 
