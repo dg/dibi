e99ce9d05352ae1b698a69aa76412b15877efeb9
David Grudl <david@grudl.com>
2014-05-13 02:47:35 +0200

    updated test support files

--- tests/dibi/bootstrap.php
+++ tests/dibi/bootstrap.php
@@ -1,13 +1,18 @@
 <?php
 
-/**
- * Test initialization and helpers.
- *
- * @author     David Grudl
- */
+// The Nette Tester command-line runner can be
+// invoked through the command: ../../vendor/bin/tester .
 
-require __DIR__ . '/../../vendor/nette/tester/Tester/bootstrap.php';
-require __DIR__ . '/../../dibi/dibi.php';
-$config = require __DIR__ . '/config.php';
+if (@!include __DIR__ . '/../../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
+}
 
+
+// configure environment
+Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
+
+
+// load connections
+$config = require __DIR__ . '/config.php';
