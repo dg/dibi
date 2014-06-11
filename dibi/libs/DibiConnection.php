c32251357dcc8a45b748ecf9bbc99b449de98be3
David Grudl <david@grudl.com>
2014-06-11 16:16:32 +0200

    fixed bug 'interface IBarPanel not found' [Closes #137]

--- dibi/libs/DibiConnection.php
+++ dibi/libs/DibiConnection.php
@@ -111,7 +111,7 @@ class DibiConnection extends DibiObject
 				$this->onEvent[] = array(new DibiFirePhpLogger($filter), 'logEvent');
 			}
 
-			if (class_exists('DibiNettePanel') && !interface_exists('Tracy\IBarPanel')) {
+			if (!interface_exists('Tracy\IBarPanel') && (interface_exists('Nette\Diagnostics\IBarPanel') || interface_exists('IBarPanel'))) {
 				$panel = new DibiNettePanel(isset($profilerCfg['explain']) ? $profilerCfg['explain'] : TRUE, $filter);
 				$panel->register($this);
 			}
