962db48043fbc2b9a364e8da70aa9c0f1dc8f5d4
David Grudl <david@grudl.com>
2015-01-13 06:58:53 +0100

    removed bridge for Nette 2.0

--- dibi/dibi.php
+++ dibi/dibi.php
@@ -34,6 +34,6 @@ require_once dirname(__FILE__) . '/libs/DibiEvent.php';
 require_once dirname(__FILE__) . '/libs/DibiFileLogger.php';
 require_once dirname(__FILE__) . '/libs/DibiFirePhpLogger.php';
 
-if (interface_exists('Nette\Diagnostics\IBarPanel') || interface_exists('IBarPanel')) {
+if (interface_exists('Nette\Diagnostics\IBarPanel')) {
 	require_once dirname(__FILE__) . '/bridges/Nette-2.1/DibiNettePanel.php';
 }
