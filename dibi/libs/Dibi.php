97b50bd2430ff15467bb16cdb2d600fda8cc871b
David Grudl <david@grudl.com>
2015-01-13 15:45:17 +0100

    Dibi: $defaultDriver changed to mysqli [Closes #156]

--- dibi/libs/Dibi.php
+++ dibi/libs/Dibi.php
@@ -68,7 +68,7 @@ class dibi
 	public static $numOfQueries = 0;
 
 	/** @var string  Default dibi driver */
-	public static $defaultDriver = 'mysql';
+	public static $defaultDriver = 'mysqli';
 
 
 	/**
