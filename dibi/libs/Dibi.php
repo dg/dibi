8f0d0fb11516f6ab1fe8616d866059550353ca00
David Grudl <david@grudl.com>
2015-10-09 12:07:38 +0200

    Released version 2.3.3

--- dibi/libs/Dibi.php
+++ dibi/libs/Dibi.php
@@ -38,8 +38,8 @@ class dibi
 		FIELD_TIME = self::TIME;
 
 	/** version */
-	const VERSION = '2.3.2',
-		REVISION = 'released on 2015-04-18';
+	const VERSION = '2.3.3',
+		REVISION = 'released on 2015-10-22';
 
 	/** sorting order */
 	const ASC = 'ASC',
