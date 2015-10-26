48adcec6dcbbbb3746d7df26cc905052b3ffb5ea
David Grudl <david@grudl.com>
2015-10-26 14:28:55 +0100

    added DibiSqlsrvDriver as alias for DibiMsSql2005Driver

---
+++ dibi/drivers/DibiSqlsrvDriver.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the "dibi" - smart database abstraction layer.
+ * Copyright (c) 2005 David Grudl (https://davidgrudl.com)
+ */
+
+
+require_once dirname(__FILE__) . '/DibiMsSql2005Driver.php';
+
+
+class DibiSqlsrvDriver extends DibiMsSql2005Driver
+{
+}
