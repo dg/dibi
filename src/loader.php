0af64b5f9b458ceca0e81ba90b233dd6f4aa9258
David Grudl <david@grudl.com>
2017-07-26 15:02:03 +0200

    Revert "loader: uses only Composer's autoloader"
    This reverts commit 53475ba05a306b9a6a201c59c127d6631d30dae0.

--- src/loader.php
+++ src/loader.php
@@ -12,7 +12,58 @@ if (PHP_VERSION_ID < 50404) {
 
 
 spl_autoload_register(function ($class) {
-	$old2new = [
+	static $map = [
+		'dibi' => 'dibi.php',
+		'Dibi\Bridges\Nette\DibiExtension22' => 'Bridges/Nette/DibiExtension22.php',
+		'Dibi\Bridges\Tracy\Panel' => 'Bridges/Tracy/Panel.php',
+		'Dibi\Connection' => 'Connection.php',
+		'Dibi\DataSource' => 'DataSource.php',
+		'Dibi\DateTime' => 'DateTime.php',
+		'Dibi\Driver' => 'interfaces.php',
+		'Dibi\DriverException' => 'exceptions.php',
+		'Dibi\Drivers\FirebirdDriver' => 'Drivers/FirebirdDriver.php',
+		'Dibi\Drivers\SqlsrvDriver' => 'Drivers/SqlsrvDriver.php',
+		'Dibi\Drivers\SqlsrvReflector' => 'Drivers/SqlsrvReflector.php',
+		'Dibi\Drivers\MsSqlDriver' => 'Drivers/MsSqlDriver.php',
+		'Dibi\Drivers\MsSqlReflector' => 'Drivers/MsSqlReflector.php',
+		'Dibi\Drivers\MySqlDriver' => 'Drivers/MySqlDriver.php',
+		'Dibi\Drivers\MySqliDriver' => 'Drivers/MySqliDriver.php',
+		'Dibi\Drivers\MySqlReflector' => 'Drivers/MySqlReflector.php',
+		'Dibi\Drivers\OdbcDriver' => 'Drivers/OdbcDriver.php',
+		'Dibi\Drivers\OracleDriver' => 'Drivers/OracleDriver.php',
+		'Dibi\Drivers\PdoDriver' => 'Drivers/PdoDriver.php',
+		'Dibi\Drivers\PostgreDriver' => 'Drivers/PostgreDriver.php',
+		'Dibi\Drivers\Sqlite3Driver' => 'Drivers/Sqlite3Driver.php',
+		'Dibi\Drivers\SqliteReflector' => 'Drivers/SqliteReflector.php',
+		'Dibi\Event' => 'Event.php',
+		'Dibi\Exception' => 'exceptions.php',
+		'Dibi\Fluent' => 'Fluent.php',
+		'Dibi\HashMap' => 'HashMap.php',
+		'Dibi\HashMapBase' => 'HashMap.php',
+		'Dibi\Helpers' => 'Helpers.php',
+		'Dibi\IDataSource' => 'interfaces.php',
+		'Dibi\Literal' => 'Literal.php',
+		'Dibi\Loggers\FileLogger' => 'Loggers/FileLogger.php',
+		'Dibi\Loggers\FirePhpLogger' => 'Loggers/FirePhpLogger.php',
+		'Dibi\NotImplementedException' => 'exceptions.php',
+		'Dibi\NotSupportedException' => 'exceptions.php',
+		'Dibi\PcreException' => 'exceptions.php',
+		'Dibi\ProcedureException' => 'exceptions.php',
+		'Dibi\Reflection\Column' => 'Reflection/Column.php',
+		'Dibi\Reflection\Database' => 'Reflection/Database.php',
+		'Dibi\Reflection\ForeignKey' => 'Reflection/ForeignKey.php',
+		'Dibi\Reflection\Index' => 'Reflection/Index.php',
+		'Dibi\Reflection\Result' => 'Reflection/Result.php',
+		'Dibi\Reflection\Table' => 'Reflection/Table.php',
+		'Dibi\Reflector' => 'interfaces.php',
+		'Dibi\Result' => 'Result.php',
+		'Dibi\ResultDriver' => 'interfaces.php',
+		'Dibi\ResultIterator' => 'ResultIterator.php',
+		'Dibi\Row' => 'Row.php',
+		'Dibi\Strict' => 'Strict.php',
+		'Dibi\Translator' => 'Translator.php',
+		'Dibi\Type' => 'Type.php',
+	], $old2new = [
 		'Dibi' => 'dibi.php',
 		'DibiColumnInfo' => 'Dibi\Reflection\Column',
 		'DibiConnection' => 'Dibi\Connection',
@@ -61,7 +112,9 @@ spl_autoload_register(function ($class) {
 		'Dibi\Drivers\MsSql2005Driver' => 'Dibi\Drivers\SqlsrvDriver',
 		'Dibi\Drivers\MsSql2005Reflector' => 'Dibi\Drivers\SqlsrvReflector',
 	];
-	if (isset($old2new[$class])) {
+	if (isset($map[$class])) {
+		require __DIR__ . '/Dibi/' . $map[$class];
+	} elseif (isset($old2new[$class])) {
 		class_alias($old2new[$class], $class);
 	}
 });
