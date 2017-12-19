db879633a44634007c1b3792218e0cb6bf9f4b04
Sean Snyder <sean@snyderitis.com>
2017-12-19 14:43:50 -0800

    Ensuring that an integer is returned, if postgres returned a string

--- src/Dibi/Drivers/PostgreDriver.php
+++ src/Dibi/Drivers/PostgreDriver.php
@@ -199,6 +199,12 @@ class PostgreDriver implements Dibi\Driver, Dibi\ResultDriver, Dibi\Reflector
 		}
 
 		$row = $res->fetch(false);
+
+		// The return value could be a string. If so, change it to an int.
+		if (is_array($row) && is_string($row[0])) {
+			$row[0] = intval($row[0]);
+		}
+
 		return is_array($row) ? $row[0] : null;
 	}
 
