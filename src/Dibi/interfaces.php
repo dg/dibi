3ccd802814d1d64eb9a4385ddf35af4d0f9ba15a
Jan Endel <pilecek@gmail.com>
2018-04-17 09:52:09 +0200

    add interface IConnection (#274)

--- src/Dibi/interfaces.php
+++ src/Dibi/interfaces.php
@@ -194,3 +194,65 @@ interface Reflector
 	 */
 	function getForeignKeys(string $table): array;
 }
+
+
+/**
+ * dibi connection.
+ */
+interface IConnection
+{
+	/**
+	 * Connects to a database.
+	 */
+	function connect(): void;
+
+	/**
+	 * Disconnects from a database.
+	 */
+	function disconnect(): void;
+
+	/**
+	 * Returns true when connection was established.
+	 */
+	function isConnected(): bool;
+
+	/**
+	 * Returns the driver and connects to a database in lazy mode.
+	 */
+	function getDriver(): Driver;
+
+	/**
+	 * Generates (translates) and executes SQL query.
+	 * @param  mixed  $args
+	 * @return Result|int   result set or number of affected rows
+	 * @throws Exception
+	 */
+	function query(...$args);
+
+	/**
+	 * Gets the number of affected rows by the last INSERT, UPDATE or DELETE query.
+	 * @throws Exception
+	 */
+	function getAffectedRows(): int;
+
+	/**
+	 * Retrieves the ID generated for an AUTO_INCREMENT column by the previous INSERT query.
+	 * @throws Exception
+	 */
+	function getInsertId(string $sequence = null): int;
+
+	/**
+	 * Begins a transaction (if supported).
+	 */
+	function begin(string $savepoint = null): void;
+
+	/**
+	 * Commits statements in a transaction.
+	 */
+	function commit(string $savepoint = null): void;
+
+	/**
+	 * Rollback changes in a transaction.
+	 */
+	function rollback(string $savepoint = null): void;
+}
