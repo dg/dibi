c3c0240e84f48e4752822e4db130847ba944bfc8
Jan Kuchař <honza.kuchar@grifart.cz>
2018-07-06 17:48:00 +0200

    PdoDriver: Only SILENT mode should be allowed as warning will not be catched

--- src/Dibi/Drivers/PdoDriver.php
+++ src/Dibi/Drivers/PdoDriver.php
@@ -72,8 +72,8 @@ class PdoDriver implements Dibi\Driver
 		$this->driverName = $this->connection->getAttribute(PDO::ATTR_DRIVER_NAME);
 		$this->serverVersion = (string) ($config['version'] ?? @$this->connection->getAttribute(PDO::ATTR_SERVER_VERSION)); // @ - may be not supported
 
-		if (!\in_array($this->connection->getAttribute(\PDO::ATTR_ERRMODE), [null, \PDO::ERRMODE_WARNING], true)) {
-			throw new Dibi\DriverException('PDO connection in other error mode then WARNING (default) is currently not supported. Consider upgrading to Dibi >=4.1.0.');
+		if ($this->connection->getAttribute(\PDO::ATTR_ERRMODE) !== \PDO::ERRMODE_SILENT)  {
+			throw new Dibi\DriverException('PDO connection in exception or warning error mode is currently not supported. Consider upgrading to Dibi >=4.1.0.');
 		}
 	}
 
