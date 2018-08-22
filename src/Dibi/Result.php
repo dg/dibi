e7539102cb62e36e95c1842af7918ebbf70f4951
Jan Kuchař <honza.kuchar@grifart.cz>
2018-08-22 15:43:11 +0200

    fix: setType should accept null as type (#309)

--- src/Dibi/Result.php
+++ src/Dibi/Result.php
@@ -501,9 +501,9 @@ class Result implements IDataSource
 
 	/**
 	 * Define column type.
-	 * @param  string  $type  use constant Type::*
+	 * @param  string|null  $type  use constant Type::*
 	 */
-	final public function setType(string $column, string $type): self
+	final public function setType(string $column, ?string $type): self
 	{
 		$this->types[$column] = $type;
 		return $this;
@@ -513,7 +513,7 @@ class Result implements IDataSource
 	/**
 	 * Returns column type.
 	 */
-	final public function getType(string $column): string
+	final public function getType(string $column): ?string
 	{
 		return $this->types[$column] ?? null;
 	}
