2e0e738b30bbcee7bcc6926e884290b721edfd28
David Grudl <david@grudl.com>
2021-03-01 19:51:41 +0100

    added PHP 8 typehints

--- src/Dibi/Drivers/FirebirdResult.php
+++ src/Dibi/Drivers/FirebirdResult.php
@@ -102,7 +102,7 @@ class FirebirdResult implements Dibi\ResultDriver
 	 * Returns the result set resource.
 	 * @return resource|null
 	 */
-	public function getResultResource()
+	public function getResultResource(): mixed
 	{
 		$this->autoFree = false;
 		return is_resource($this->resultSet) ? $this->resultSet : null;
