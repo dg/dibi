361b0fe1682400f1e0ce4c9ba1df70d72ac219f3
David Grudl <david@grudl.com>
2021-12-12 17:40:23 +0100

    added PHP 8 typehints

--- src/Dibi/Drivers/OdbcDriver.php
+++ src/Dibi/Drivers/OdbcDriver.php
@@ -170,7 +170,7 @@ class OdbcDriver implements Dibi\Driver
 	 * Returns the connection resource.
 	 * @return resource|null
 	 */
-	public function getResource()
+	public function getResource(): mixed
 	{
 		return is_resource($this->connection) ? $this->connection : null;
 	}
