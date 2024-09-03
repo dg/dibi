a6712c0f52f1a2b9ac96c02197fd7a7d6237636f
David Grudl <david@grudl.com>
2024-09-03 16:31:38 +0200

    uses PHP 5.2 features

--- src/Dibi/Bridges/Tracy/Panel.php
+++ src/Dibi/Bridges/Tracy/Panel.php
@@ -35,8 +35,8 @@ class Panel implements Tracy\IBarPanel
 	public function register(Dibi\Connection $connection): void
 	{
 		Tracy\Debugger::getBar()->addPanel($this);
-		Tracy\Debugger::getBlueScreen()->addPanel([self::class, 'renderException']);
-		$connection->onEvent[] = [$this, 'logEvent'];
+		Tracy\Debugger::getBlueScreen()->addPanel(self::renderException(...));
+		$connection->onEvent[] = $this->logEvent(...);
 	}
 
 
