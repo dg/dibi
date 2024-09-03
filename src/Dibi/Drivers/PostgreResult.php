509a173d2d5337afb10c70ca45bc67492de0fc86
David Grudl <david@grudl.com>
2024-09-03 16:31:38 +0200

    uses PHP 5.2 features

--- src/Dibi/Drivers/PostgreResult.php
+++ src/Dibi/Drivers/PostgreResult.php
@@ -20,8 +20,7 @@ use PgSql;
 class PostgreResult implements Dibi\ResultDriver
 {
 	public function __construct(
-		/** @var resource|PgSql\Result */
-		private $resultSet,
+		private PgSql\Result $resultSet,
 	) {
 	}
 
@@ -88,13 +87,10 @@ class PostgreResult implements Dibi\ResultDriver
 
 	/**
 	 * Returns the result set resource.
-	 * @return resource|PgSql\Result|null
 	 */
-	public function getResultResource(): mixed
+	public function getResultResource(): PgSql\Result
 	{
-		return is_resource($this->resultSet) || $this->resultSet instanceof PgSql\Result
-			? $this->resultSet
-			: null;
+		return $this->resultSet;
 	}
 
 
