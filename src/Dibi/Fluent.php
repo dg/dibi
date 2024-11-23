527feaa91446da89d0a9ff812a863d9267e5910c
David Grudl <david@grudl.com>
2024-11-23 17:59:08 +0100

    used attribute Deprecated

--- src/Dibi/Fluent.php
+++ src/Dibi/Fluent.php
@@ -50,7 +50,7 @@ class Fluent implements IDataSource
 		Identifier = 'n',
 		Remove = false;
 
-	/** @deprecated use Fluent::Remove */
+	#[\Deprecated('use Fluent::Remove')]
 	public const REMOVE = self::Remove;
 
 	public static array $masks = [
